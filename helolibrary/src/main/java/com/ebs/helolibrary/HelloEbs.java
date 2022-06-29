7dd6c27017f7c09c461388941e26a063996bc83a
estiak-sourav <easourav@gmail.com>
2022-06-29 15:21:45 +0600

    Initial commit

---
+++ helolibrary/src/main/java/com/ebs/helolibrary/HelloEbs.java
@@ -0,0 +1,15 @@
+package com.ebs.helolibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class HelloEbs {
+
+    public static void showToast (Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static int addon(int a, int b){
+        return a + b;
+    }
+}
